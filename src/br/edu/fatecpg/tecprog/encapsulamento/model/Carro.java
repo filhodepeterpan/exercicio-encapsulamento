2b2b28fefc419dcdf16aafb3725da71f973928e5
Douglas Soares <douglassoares.cinema@gmail.com>
2026-03-27 19:58:08 -0300

    add_class_Carro

--- src/br/edu/fatecpg/tecprog/encapsulamento/model/Carro.java
+++ src/br/edu/fatecpg/tecprog/encapsulamento/model/Carro.java
@@ -1,2 +1,47 @@
-package br.edu.fatecpg.tecprog.encapsulamento.model;public class Carro {
+package br.edu.fatecpg.tecprog.encapsulamento.model;
+
+public class Carro {
+    private String marca;
+    private String modelo;
+    private String placa;
+    private double capacidadeTanque;
+    private double tanque;
+
+    public Carro(String marca, String modelo, double capacidadeTanque, double tanque){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+        this.tanque = tanque;
+    }
+
+    // GETTERS E SETTERS
+    public String getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    public String getPlaca() { return placa; }
+    public void setPlaca(String placa) { this.placa = placa; }
+
+    public double getCapacidadeTanque() { return capacidadeTanque; }
+
+    public double getTanque() { return tanque; }
+
+    // OUTROS MÉTODOS
+
+    private double combustivelFaltando(){
+        return this.capacidadeTanque - tanque;
+    }
+
+    public String verificaCombustivel(){
+        int porcentagem = (int)((tanque / capacidadeTanque) * 100);
+
+        return "\nCombustível atual: %.2fL \nCombustível que pode ser abastecido: %.2fL \n%d%% cheio!\n"
+                .formatted(this.tanque, combustivelFaltando(), porcentagem);
+    }
+
+    public void abastecer(double combustivel){
+        tanque += combustivelFaltando() > combustivel ? combustivel : combustivelFaltando();
+    }
 }
--- src/br/edu/fatecpg/tecprog/encapsulamento/view/Main.java
+++ src/br/edu/fatecpg/tecprog/encapsulamento/view/Main.java
@@ -4,6 +4,14 @@ import br.edu.fatecpg.tecprog.encapsulamento.model.*;
 public class Main {
 
     public static void main (String [] args){
+        Carro carro = new Carro("Hyundai", "HB20",50, 25);
 
+        System.out.println(carro.verificaCombustivel());
+
+        carro.abastecer(15.4);
+        System.out.println(carro.verificaCombustivel());
+
+        carro.abastecer(20.3);
+        System.out.println(carro.verificaCombustivel());
     }
 }
